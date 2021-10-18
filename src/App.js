7d1f8d4252f93a8ff0d64728a0ad8ff813c67354
FranMedic <m.francisca.medico@gmail.com>
2021-10-18 17:10:22 +0200

    added circle class and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Circle from "./components/Circle.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const circulos = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
+  return circulos.map((circulo) => <Circle key={circulo.id} />);
+}
 export default App;
